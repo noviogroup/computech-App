d5e9b12c011e32174c1fb0947578992b9a60c92d
noviogroup <noviogroup@gmail.com>
2020-05-20 10:19:02 -0400

    changes made.
    just solve short_description swap
    
    Signed-off-by: noviogroup <noviogroup@gmail.com>

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API = 'http://computechenterprise.com/';
+export const API = 'https://computechenterprise.com';
 
 export const CONSUMER_KEY = 'ck_d7150cf56dba75b137ab1d0017d16f8c195a7b15';
 export const CONSUMER_SECRET = 'cs_f0d72628d137e83144cc133f5c7e0eff458d00eb';
